e2b97bc10167a35f7caaf036dac06c6e47cba67b
Shwetha A <shwethaadiraj@gmail.com>
2025-12-25 23:02:04 +0530

    screensaver color routing

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import ScreenSaver from "../../../components/ScreenSaver";
+import Link from "next/link";
 
-function ScreenSaverExercise() {
+function ScreenSaverHome() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p> Choose screen saver color: </p>
+      <ul>
+        <Link href={"/exercises/01-screensaver/pink"}>
+          <li>pink</li>
+        </Link>
+        <Link href={"/exercises/01-screensaver/slateblue"}>
+          <li>slateblue</li>
+        </Link>
+        <Link href={"/exercises/01-screensaver/violet"}>
+          <li>violet</li>
+        </Link>
+        <Link href={"/exercises/01-screensaver/white"}>
+          <li>white</li>
+        </Link>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverHome;
